a3a4f5b207bfcc5f301041176fe386b20c624f40
emu <an_unidentified_emu@gmail.com>
2021-08-01 11:10:23 -0400

    first draft of bowser 1

---
+++ levels/bitdw/geo.inc.c
@@ -0,0 +1,1 @@
+#include "levels/bitdw/area_1/geo.inc.c"
---
+++ levels/bitdw/leveldata.inc.c
@@ -0,0 +1,4 @@
+#include "levels/bitdw/area_1/collision.inc.c"
+#include "levels/bitdw/area_1/macro.inc.c"
+#include "levels/bitdw/area_1/spline.inc.c"
+#include "levels/bitdw/model.inc.c"
