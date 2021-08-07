4eb0446f9bafcaeea12bf6b4ef09dcd56c5ac839
emu <an_unidentified_emu@gmail.com>
2021-08-06 23:25:13 -0400

    added textures and more level

--- levels/bitdw/leveldata.inc.c
+++ levels/bitdw/leveldata.inc.c
@@ -1,3 +1,4 @@
+#include "levels/bitdw/texture_include.inc.c"
 #include "levels/bitdw/area_1/collision.inc.c"
 #include "levels/bitdw/area_1/macro.inc.c"
 #include "levels/bitdw/area_1/spline.inc.c"
---
+++ levels/bitdw/texscroll.inc.h
@@ -0,0 +1,8 @@
+extern void scroll_bitdw_dl_final_pipe_torus_mesh_layer_1_vtx_0();
+extern void scroll_bitdw_dl_first_torus_mesh_layer_1_vtx_0();
+extern void scroll_bitdw_dl_forth_torus_mesh_layer_1_vtx_0();
+extern void scroll_bitdw_dl_second_torus_mesh_layer_1_vtx_0();
+extern void scroll_bitdw_dl_second_torus_mesh_layer_1_vtx_1();
+extern void scroll_bitdw_dl_second_torus_mesh_layer_1_vtx_2();
+extern void scroll_bitdw_dl_third_torus_mesh_layer_1_vtx_0();
+extern void scroll_bitdw();
