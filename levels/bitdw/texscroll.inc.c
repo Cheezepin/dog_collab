f84a63c25018c8162fb1738a3206587e02f748e0
emu <an_unidentified_emu@gmail.com>
2021-08-17 21:33:01 -0400

    just lots of level changes

--- levels/bitdw/texscroll.inc.c
+++ levels/bitdw/texscroll.inc.c
@@ -1,14 +1,14 @@
-void scroll_bitdw_dl_final_pipe_torus_mesh_layer_1_vtx_0() {
+void scroll_bitdw_dl_Cube_004_mesh_layer_5_vtx_0() {
 	int i = 0;
-	int count = 261;
+	int count = 24;
 	int width = 32 * 0x20;
 	int height = 32 * 0x20;
 
 	static int currentY = 0;
 	int deltaY;
-	Vtx *vertices = segmented_to_virtual(bitdw_dl_final_pipe_torus_mesh_layer_1_vtx_0);
+	Vtx *vertices = segmented_to_virtual(bitdw_dl_Cube_004_mesh_layer_5_vtx_0);
 
-	deltaY = (int)(0.1899999976158142 * 0x20) % height;
+	deltaY = (int)(0.19000044465065002 * 0x20) % height;
 
 	if (absi(currentY) > height) {
 		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
@@ -22,7 +22,7 @@ void scroll_bitdw_dl_final_pipe_torus_mesh_layer_1_vtx_0() {
 }
 void scroll_bitdw_dl_first_torus_mesh_layer_1_vtx_0() {
 	int i = 0;
-	int count = 494;
+	int count = 415;
 	int width = 32 * 0x20;
 	int height = 32 * 0x20;
 
@@ -44,7 +44,7 @@ void scroll_bitdw_dl_first_torus_mesh_layer_1_vtx_0() {
 }
 void scroll_bitdw_dl_forth_torus_mesh_layer_1_vtx_0() {
 	int i = 0;
-	int count = 437;
+	int count = 410;
 	int width = 32 * 0x20;
 	int height = 32 * 0x20;
 
@@ -66,7 +66,7 @@ void scroll_bitdw_dl_forth_torus_mesh_layer_1_vtx_0() {
 }
 void scroll_bitdw_dl_second_torus_mesh_layer_1_vtx_0() {
 	int i = 0;
-	int count = 365;
+	int count = 332;
 	int width = 32 * 0x20;
 	int height = 32 * 0x20;
 
@@ -86,17 +86,17 @@ void scroll_bitdw_dl_second_torus_mesh_layer_1_vtx_0() {
 	currentY += deltaY;
 
 }
-void scroll_bitdw_dl_second_torus_mesh_layer_1_vtx_1() {
+void scroll_bitdw_dl_TOWER_mesh_layer_5_vtx_1() {
 	int i = 0;
-	int count = 26;
+	int count = 427;
 	int width = 32 * 0x20;
 	int height = 32 * 0x20;
 
 	static int currentY = 0;
 	int deltaY;
-	Vtx *vertices = segmented_to_virtual(bitdw_dl_second_torus_mesh_layer_1_vtx_1);
+	Vtx *vertices = segmented_to_virtual(bitdw_dl_TOWER_mesh_layer_5_vtx_1);
 
-	deltaY = (int)(1.0299999713897705 * 0x20) % height;
+	deltaY = (int)(0.19000044465065002 * 0x20) % height;
 
 	if (absi(currentY) > height) {
 		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
@@ -108,57 +108,61 @@ void scroll_bitdw_dl_second_torus_mesh_layer_1_vtx_1() {
 	currentY += deltaY;
 
 }
-void scroll_bitdw_dl_second_torus_mesh_layer_1_vtx_2() {
+void scroll_bitdw_dl_water_bottom_mesh_layer_5_vtx_0() {
 	int i = 0;
-	int count = 26;
-	int width = 64 * 0x20;
-	int height = 64 * 0x20;
+	int count = 23;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
 
 	static int currentX = 0;
 	int deltaX;
-	static int timeX;
-	float amplitudeX = 1.0;
-	float frequencyX = 1.0;
-	float offsetX = 0.0;
-	static int currentY = 0;
-	int deltaY;
-	static int timeY;
-	float amplitudeY = 1.0;
-	float frequencyY = 1.0;
-	float offsetY = 0.0;
-	Vtx *vertices = segmented_to_virtual(bitdw_dl_second_torus_mesh_layer_1_vtx_2);
+	Vtx *vertices = segmented_to_virtual(bitdw_dl_water_bottom_mesh_layer_5_vtx_0);
 
-	deltaX = (int)(amplitudeX * frequencyX * coss((frequencyX * timeX + offsetX) * (1024 * 16 - 1) / 6.28318530718) * 0x20);
-	deltaY = (int)(amplitudeY * frequencyY * coss((frequencyY * timeY + offsetY) * (1024 * 16 - 1) / 6.28318530718) * 0x20);
+	deltaX = (int)(0.10000002384185791 * 0x20) % width;
 
 	if (absi(currentX) > width) {
 		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
 	}
-	if (absi(currentY) > height) {
-		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+
+}
+void scroll_bitdw_dl_water_top_mesh_layer_1_vtx_0() {
+	int i = 0;
+	int count = 23;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(bitdw_dl_water_top_mesh_layer_1_vtx_0);
+
+	deltaX = (int)(0.10000002384185791 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
 	}
 
 	for (i = 0; i < count; i++) {
 		vertices[i].n.tc[0] += deltaX;
-		vertices[i].n.tc[1] += deltaY;
 	}
 	currentX += deltaX;
-	timeX += 1;
-	currentY += deltaY;
-	timeY += 1;
 
 }
-void scroll_bitdw_dl_third_torus_mesh_layer_1_vtx_0() {
+void scroll_bitdw_dl_water_top_fake_mesh_layer_5_vtx_0() {
 	int i = 0;
-	int count = 725;
+	int count = 23;
 	int width = 32 * 0x20;
 	int height = 32 * 0x20;
 
 	static int currentY = 0;
 	int deltaY;
-	Vtx *vertices = segmented_to_virtual(bitdw_dl_third_torus_mesh_layer_1_vtx_0);
+	Vtx *vertices = segmented_to_virtual(bitdw_dl_water_top_fake_mesh_layer_5_vtx_0);
 
-	deltaY = (int)(0.1899999976158142 * 0x20) % height;
+	deltaY = (int)(0.19000044465065002 * 0x20) % height;
 
 	if (absi(currentY) > height) {
 		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
@@ -171,11 +175,12 @@ void scroll_bitdw_dl_third_torus_mesh_layer_1_vtx_0() {
 
 }
 void scroll_bitdw() {
-	scroll_bitdw_dl_final_pipe_torus_mesh_layer_1_vtx_0();
+	scroll_bitdw_dl_Cube_004_mesh_layer_5_vtx_0();
 	scroll_bitdw_dl_first_torus_mesh_layer_1_vtx_0();
 	scroll_bitdw_dl_forth_torus_mesh_layer_1_vtx_0();
 	scroll_bitdw_dl_second_torus_mesh_layer_1_vtx_0();
-	scroll_bitdw_dl_second_torus_mesh_layer_1_vtx_1();
-	scroll_bitdw_dl_second_torus_mesh_layer_1_vtx_2();
-	scroll_bitdw_dl_third_torus_mesh_layer_1_vtx_0();
+	scroll_bitdw_dl_TOWER_mesh_layer_5_vtx_1();
+	scroll_bitdw_dl_water_bottom_mesh_layer_5_vtx_0();
+	scroll_bitdw_dl_water_top_mesh_layer_1_vtx_0();
+	scroll_bitdw_dl_water_top_fake_mesh_layer_5_vtx_0();
 }
