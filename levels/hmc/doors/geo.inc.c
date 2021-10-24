281fb99f5343cc299615c49f214d167699fdd80a
Fazana <52551480+FazanaJ@users.noreply.github.com>
2021-10-24 16:35:11 +0100

    ok

--- levels/hmc/doors/geo.inc.c
+++ levels/hmc/doors/geo.inc.c
@@ -3,10 +3,13 @@
 const GeoLayout doors_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
-		GEO_DISPLAY_LIST(LAYER_OPAQUE, doors_Cube_008_mesh_layer_1),
-		GEO_DISPLAY_LIST(LAYER_ALPHA, doors_Cube_008_mesh_layer_4),
-		GEO_DISPLAY_LIST(LAYER_OPAQUE, doors_material_revert_render_settings),
-		GEO_DISPLAY_LIST(LAYER_ALPHA, doors_material_revert_render_settings),
+        GEO_CULLING_RADIUS(20000),
+        GEO_OPEN_NODE(),
+            GEO_DISPLAY_LIST(LAYER_OPAQUE, doors_Cube_008_mesh_layer_1),
+            GEO_DISPLAY_LIST(LAYER_ALPHA, doors_Cube_008_mesh_layer_4),
+            GEO_DISPLAY_LIST(LAYER_OPAQUE, doors_material_revert_render_settings),
+            GEO_DISPLAY_LIST(LAYER_ALPHA, doors_material_revert_render_settings),
+        GEO_CLOSE_NODE(),
 	GEO_CLOSE_NODE(),
 	GEO_END(),
 };
