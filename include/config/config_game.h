d145f5866fa4f8f26589a8e5eb183d6fc97fe944
thecozies <collinpferguson@gmail.com>
2022-01-11 18:09:11 -0600

    increase model count

--- include/config/config_game.h
+++ include/config/config_game.h
@@ -43,7 +43,7 @@
 // #define GLOBAL_STAR_IDS
 
 // Number of possible unique model ID's (keep it higher than 256)
-#define MODEL_ID_COUNT 256
+#define MODEL_ID_COUNT 0x200
 
 // Number of supported areas per level.
 #define AREA_COUNT 8
