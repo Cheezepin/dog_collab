75519298ad1e9a83bc6e6fcdb6acdd5b69496db9
someone2639 <someone2639@gmail.com>
2022-04-09 23:18:08 -0400

    the new new

---
+++ actors/turnstile/geo_header.h
@@ -0,0 +1,14 @@
+extern const GeoLayout turnstile_geo[];
+extern Lights1 turnstile_tsile_lights;
+extern Vtx turnstile_TurnStile_mesh_layer_1_vtx_cull[8];
+extern Vtx turnstile_TurnStile_mesh_layer_1_vtx_0[200];
+extern Gfx turnstile_TurnStile_mesh_layer_1_tri_0[];
+extern Vtx turnstile_TurnStile_mesh_layer_5_vtx_cull[8];
+extern Vtx turnstile_TurnStile_mesh_layer_5_vtx_0[32];
+extern Gfx turnstile_TurnStile_mesh_layer_5_tri_0[];
+extern Gfx mat_turnstile_tsile[];
+extern Gfx mat_revert_turnstile_tsile[];
+extern Gfx mat_turnstile_another_wndo[];
+extern Gfx turnstile_TurnStile_mesh_layer_1[];
+extern Gfx turnstile_TurnStile_mesh_layer_5[];
+extern Gfx turnstile_material_revert_render_settings[];
--- levels/bob/area_3/geo.inc.c
+++ levels/bob/area_3/geo.inc.c
@@ -3,7 +3,6 @@
 const GeoLayout bob_area_3_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
-		GEO_TRANSLATE_ROTATE_WITH_DL(LAYER_OPAQUE, 277, -47, 1, 0, -180, 0, bob_dl_Plane_001_mesh_layer_1),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
 };
@@ -21,7 +20,7 @@ const GeoLayout bob_area_3[] = {
 		GEO_OPEN_NODE(),
 			GEO_CAMERA_FRUSTUM_WITH_FUNC(45.0000, 100, 30000, geo_camera_fov),
 			GEO_OPEN_NODE(),
-				GEO_CAMERA(CAMERA_MODE_8_DIRECTIONS, 0, 0, 45969, 0, -100, 45969, geo_camera_main),
+				GEO_CAMERA(CAMERA_MODE_8_DIRECTIONS, 46370, 0, 0, 46370, -100, 0, geo_camera_main),
 				GEO_OPEN_NODE(),
 					GEO_BRANCH(1, bob_area_3_geo),
 					GEO_RENDER_OBJ(),
@@ -29,7 +28,6 @@ const GeoLayout bob_area_3[] = {
 				GEO_CLOSE_NODE(),
 			GEO_CLOSE_NODE(),
 		GEO_CLOSE_NODE(),
-		GEO_DISPLAY_LIST(LAYER_OPAQUE, bob_dl_material_revert_render_settings),
 	GEO_CLOSE_NODE(),
 	GEO_END(),
 };
