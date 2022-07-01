d5a35a87119f3039a094cfdda3397bc7b78e1026
Cheezepin <cheezepin@gmail.com>
2022-06-30 21:51:25 -0700

    most of bowser level, little bit of bowser boss

--- levels/bitfs/cheeze_bombwall2/collision.inc.c
+++ levels/bitfs/cheeze_bombwall2/collision.inc.c
@@ -5,7 +5,7 @@ const Collision cheeze_bombwall2_collision[] = {
 	COL_VERTEX(484, 0, -96),
 	COL_VERTEX(-484, 0, 96),
 	COL_VERTEX(-484, 1250, 96),
-	COL_TRI_INIT(SURFACE_DEFAULT, 2),
+	COL_TRI_INIT(SURFACE_NOT_SLIPPERY, 2),
 	COL_TRI(0, 1, 2),
 	COL_TRI(0, 2, 3),
 	COL_TRI_STOP(),
