af1da5cef0c82de3190894ad95f9a77212035225
Beverly <105609601+BeverlyBean@users.noreply.github.com>
2022-12-12 15:56:04 -0500

    latest changes as of dec. 12

--- src/game/texscroll/wf_texscroll.inc.c
+++ src/game/texscroll/wf_texscroll.inc.c
@@ -1,11 +1,9 @@
 #include "levels/wf/header.h"
 #include "levels/wf/texscroll.inc.c"
 #include "levels/wf/atoll_ocean/texscroll.inc.c"
-#include "levels/wf/atoll_skybox/texscroll.inc.c"
 #include "levels/wf/ktq_ring/texscroll.inc.c"
 void scroll_textures_wf() {
 			scroll_wf();
 		scroll_wf_level_dl_atoll_ocean();
-	scroll_wf_level_dl_atoll_skybox();
-	scroll_wf_level_geo_ktq_ring();
+		scroll_wf_level_geo_ktq_ring();
 }
