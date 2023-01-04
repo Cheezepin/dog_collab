3630a73db3243bbe6ff6a814c8cd9eef39d6673a
thecozies <collinpferguson@gmail.com>
2023-01-04 09:02:32 -0600

    new power meter

---
+++ actors/life_meter_new/header.h
@@ -0,0 +1,12 @@
+extern Gfx life_meter_new_init[];
+extern Gfx life_meter_tex_init_full[];
+extern Gfx life_meter_tex_init_half[];
+extern Gfx life_meter_tex_init_empty[];
+
+extern Gfx life_meter_revert_material[];
+
+extern Gfx life_meter_quad_tri_0[];
+extern Gfx life_meter_quad_tri_1[];
+extern Gfx life_meter_quad_tri_2[];
+extern Gfx life_meter_quad_tri_3[];
+extern Gfx *life_meter_tex_lut[];
