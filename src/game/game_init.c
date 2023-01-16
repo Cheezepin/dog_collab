468bb3659065d09b9e96f698ea6bbb02f25df035
thecozies <collinpferguson@gmail.com>
2023-01-16 10:05:11 -0600

    reduce controller polling to 2 max

--- src/game/game_init.c
+++ src/game/game_init.c
@@ -97,6 +97,8 @@ struct Controller *gPlayer1Controller = &gControllers[0];
 struct Controller *gPlayer2Controller = &gControllers[1];
 struct Controller *gPlayer3Controller = &gControllers[2]; // Probably debug only, see note below
 
+s32 sMaxContPort = 0;
+
 // Title Screen Demo Handler
 struct DemoInput *gCurrDemoInput = NULL;
 u16 gDemoInputListID = 0;
@@ -667,7 +669,8 @@ void read_controller_inputs(s32 threadID) {
     run_demo_inputs();
 #endif
 
-    for (i = 0; i < 2; i++) {
+    s32 numPorts = MIN(sMaxContPort, 2);
+    for (i = 0; i < numPorts; i++) {
         struct Controller *controller = &gControllers[i];
 
         // if we're receiving inputs, update the controller struct with the new button info.
@@ -723,12 +726,13 @@ void init_controllers(void) {
     gSramProbe = nuPiInitSram();
 #endif
 
+    s32 maxPort = 0;
     // Loop over the 4 ports and link the controller structs to the appropriate
     // status and pad. Interestingly, although there are pointers to 3 controllers,
     // only 2 are connected here. The third seems to have been reserved for debug
     // purposes and was never connected in the retail ROM, thus gPlayer3Controller
     // cannot be used, despite being referenced in various code.
-    for (cont = 0, port = 0; port < 4 && cont < 2; port++) {
+    for (cont = 0, port = 0; port < MAXCONTROLLERS && cont < 2; port++) {
         // Is controller plugged in?
         if (gControllerBits & (1 << port)) {
             // The game allows you to have just 1 controller plugged
@@ -740,8 +744,12 @@ void init_controllers(void) {
 #endif
             gControllers[cont].statusData = &gControllerStatuses[port];
             gControllers[cont++].controllerData = &gControllerPads[port];
+            maxPort = port;
         }
     }
+    sMaxContPort = MIN(maxPort+1, MAXCONTROLLERS);
+    osSyncPrintf("sMaxContPort %d\n", sMaxContPort);
+    osContSetCh(sMaxContPort);
 }
 
 // Game thread core
