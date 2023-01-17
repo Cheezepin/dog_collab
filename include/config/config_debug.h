cbfabc5e7b6779696e84d233e58342a1996e5b9a
emu <an_unidentified_emu@gmail.com>
2023-01-17 18:29:43 -0500

    put debug back to normal

--- include/config/config_debug.h
+++ include/config/config_debug.h
@@ -9,7 +9,7 @@
 #endif
 
 // Enables most debug options, including PUPPYPRINT_DEBUG.
- #define DEBUG_ALL
+// #define DEBUG_ALL
 
 // Disables all debug options (except PUPPYPRINT)
 // #define DISABLE_ALL
@@ -22,7 +22,7 @@
 // Uncomment this define and set a test level in order to boot straight into said level.
 // This allows you to quickly test the level you're working on.
 // If you want the game to boot normally, just comment out the define again.
- #define TEST_LEVEL LEVEL_BOWSER_1
+// #define TEST_LEVEL LEVEL_CASTLE_GROUNDS
 
 // Enable debug level select. Hold L while the game boots to turn it on.
 // #define DEBUG_LEVEL_SELECT
