394d76e85175d0fd56a494d02038114e9f9f3ff5
Gregory Heskett <gheskett@gmail.com>
2023-02-14 02:43:52 -0500

    Undo debug defines

--- include/config/config_debug.h
+++ include/config/config_debug.h
@@ -16,7 +16,7 @@
 
 // Enables a comprehensive standalone profiler. Automatically enabled by PUPPYPRINT_DEBUG.
 // If not using PUPPYPRINT_DEBUG, press L to toggle the profiler.
-#define USE_PROFILER
+// #define USE_PROFILER
 
 // TEST LEVEL
 // Uncomment this define and set a test level in order to boot straight into said level.
@@ -28,10 +28,10 @@
 // #define DEBUG_LEVEL_SELECT
 
 // Enables debug free move (D-pad up to enter, A to exit).
-#define ENABLE_DEBUG_FREE_MOVE
+// #define ENABLE_DEBUG_FREE_MOVE
 
 // Include a custom, enhanced performance profiler. (Enables PUPPYPRINT by default in config_safeguards).
-#define PUPPYPRINT_DEBUG 1
+// #define PUPPYPRINT_DEBUG 1
 
 // Press D-pad left to show the debug UI.
 // #define VANILLA_STYLE_CUSTOM_DEBUG
@@ -45,7 +45,7 @@
 
 // Same as above, but also reads all save file flags as complete.
 // This will not overwrite existing save file data unless you save over it.
-#define COMPLETE_SAVE_FILE
+// #define COMPLETE_SAVE_FILE
 
 // Removes the limit on FPS.
 // #define UNLOCK_FPS
@@ -59,5 +59,6 @@
 // Doesn't set any flags, but skips the tutorial.
 // #define SKIP_TUTORIAL
 
-// Haha console audio go brrr
+// Haha console audio go brrr (also convenient toggle between full and light 2639 sequences for console, with this define representing full version when enabled).
+// (This should probably be renamed to something else. Potentially moved to audio config too, though note count also relies on this define FYI.)
 #define AUDIO_CRACKLE_DEBUGGING
