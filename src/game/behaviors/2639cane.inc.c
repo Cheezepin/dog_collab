bd3dcbb16729b4cbc90026037298c9599f56476f
someone2639 <someone2639@gmail.com>
2023-02-24 00:41:16 -0500

    quick act 1 bugfix

--- src/game/behaviors/2639cane.inc.c
+++ src/game/behaviors/2639cane.inc.c
@@ -2,8 +2,14 @@ void bhv_2639cane_init(void) {
 	
 }
 void bhv_2639cane_loop(void) {
+	struct Object *doge = cur_obj_nearest_object_with_behavior(bhvDoginlobby);
+
+	if (doge == NULL) return;
+
 	if (o->oDistanceToMario <= 100) {
-		if (gMarioState->action == ACT_GROUND_POUND_LAND) {
+		if (gMarioState->action == ACT_GROUND_POUND_LAND
+
+		&& doge->oAction == 4) { // only gp if doggo
 			_2639_BoB_A1_CaneCollected = 1;
 			MakeCoinWithModel(o, 1, MODEL_CANE2639);
 			mark_obj_for_deletion(o);
